3a44623bc071c3f95e89437453a841f344bf6561
José Ignacio Escribano <jiep12345@hotmail.com>
2018-11-01 15:35:24 +0100

    Add OpenCV

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,38 @@
   div(id="app")
     Bar/
     .content
+      .notification.is-success(v-if="isCv") OpenCV is loaded
+      .notification.is-danger(v-else="isCv") OpenCV is not loaded
       router-view/
 </template>
 
 <script>
 import Bar from '@/components/Bar'
+const cv =  require('opencv4nodejs')
+import * as path from 'path'
+import * as fs from 'fs'
+
+import '@/js/image-helpers'
 
 export default {
   name: 'app',
   components: {
     Bar
+  },
+  data() {
+    return {
+      isCv: null
+    }
+  },
+  mounted() {
+    if (!cv.xmodules.face) {
+      throw new Error('exiting: opencv4nodejs compiled without face module');
+      this.isCv = false;
+    }else{
+      this.isCv = true;
+    }
+    const classifier = new cv.CascadeClassifier('public/HAAR_FRONTALFACE_ALT2.xml');
+    console.log(classifier);
   }
 }
 </script>
